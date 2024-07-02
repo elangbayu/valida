7cd2dc034b1e532dd8acc32329b3d1c02676dd10
elangbayu <elangbayu17@gmail.com>
2024-07-02 20:09:26 +0700

    feat: get params and req body, also logging

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "valida/cmd"
+import (
+	"log"
+	"valida/cmd"
+	"valida/internal/apitest"
+)
 
 func main() {
+	err := apitest.InitializeLogFile("api_test_log.json")
+	if err != nil {
+		log.Fatalf("Failed to initialize log file: %v", err)
+	}
+	defer func() {
+		if err := apitest.CloseLogFile(); err != nil {
+			log.Printf("Failed to close log file: %v", err)
+		}
+	}()
+
 	cmd.Execute()
 }
